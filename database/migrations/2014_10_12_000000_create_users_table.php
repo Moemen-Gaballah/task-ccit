15360982b804ec2904db70b26af53412ffa5eb45
Moemen Gaballa <moemengaballa@gmail.com>
2022-03-25 09:32:13 +0200

    install Yajra\DataTables - ui authentication - add fields to user migration file - add seeder for add admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('type', ['customer', 'admin'])->default('customer');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
